fc0b639866877d35faeddcdbba7d16ca5dc7622c
MuhammadRehanQadri <rehan.code@gmail.com>
2021-05-26 15:02:58 +0500

    Init

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  // form!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+  initializeForm() {
+    // this.form = this.formBuilder.group({
+    //   username: ['', Validators.required],
+    //   email: ['', Validators.required],
+    //   password: ['', Validators.required],
+    // })
+  }
+
+  onFormSubmit(){
+    // console.log('form', this.form.value)
+  }
+}
