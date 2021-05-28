57db96c59d94c71addbbcb9c992377fad004bac6
MuhammadRehanQadri <rehan.code@gmail.com>
2021-05-28 15:11:03 +0500

    On successful registration (registerSuccessAction), redirect page to '/'

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
 import { of } from 'rxjs'
-import { catchError, map, switchMap } from 'rxjs/operators'
+import { catchError, map, switchMap, tap } from 'rxjs/operators'
 import { HttpErrorResponse } from '@angular/common/http'
 
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
@@ -11,15 +11,16 @@ import {
   registerFailureAction,
   registerSuccessAction,
 } from '../actions/register.action'
-import { RegisterRequestInterface } from '../../types/registerRequest.interface'
 import { PersistanceService } from 'src/app/shared/services/persistance.service'
+import { Router } from '@angular/router'
 
 @Injectable()
 export class RegisterEffect {
   constructor(
     private actions$: Actions,
     private authService: AuthService,
-    private persistanceService: PersistanceService
+    private persistanceService: PersistanceService,
+    private router: Router
   ) {}
 
   register$ = createEffect(() =>
@@ -40,5 +41,16 @@ export class RegisterEffect {
         )
       })
     )
+  )
+
+  redirect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(({ currentUser }) => {
+          this.router.navigateByUrl('/')
+        })
+      ),
+    { dispatch: false }
   )
 }
